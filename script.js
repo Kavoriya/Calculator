c81e7146edca0ca89e8af61ed01488b55e9cf70c
Roman <kavoriya@gmail.com>
2023-01-01 19:56:31 +0500

    Build: show digits on the display
    Digits now appear on the display and
    get added to variable calculatorValue.

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function divide(a, b) {
 function operate(func, a, b) {
   return func(a, b);
 }
+
+let calculatorValue = "";
+let display = document.querySelector("#display");
+
+let digits = [];
+let domDigits = document.getElementsByClassName("digit");
+[...domDigits].forEach(item => {
+  digits.push(item)
+});
+
+digits.forEach(digit => {
+  digit.addEventListener("click", () => {
+    calculatorValue += digit.value;
+    display.textContent += digit.value;
+  })
+});
