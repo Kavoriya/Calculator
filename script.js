f4acbbabeec715cdc24b2151a9a608d9578642e2
Roman <kavoriya@gmail.com>
2023-01-02 16:17:52 +0500

    Build: add operate function, works using an array
    The operate function only works with "add". Array is used,
    which contains numbers and operators. Several "add"
    operators can be used in one equation.

--- script.js
+++ script.js
@@ -15,11 +15,12 @@ function divide(a, b) {
 }
 
 function operate(func, a, b) {
-  return func(a, b);
+  return func(+a, +b);
 }
 
 let calculatorValue = "";
 let display = document.querySelector("#display");
+let equation = []; // this array holds numbers and operators
 
 let digits = [];
 let domDigits = document.getElementsByClassName("digit");
@@ -32,4 +33,23 @@ digits.forEach(digit => {
     calculatorValue += digit.value;
     display.textContent += digit.value;
   })
+});
+
+let addButton = document.querySelector("#btn-add"); 
+addButton.addEventListener("click", () => { 
+  equation.push(calculatorValue);
+  calculatorValue = "";
+  equation.push(add);
+  display.textContent += "+";
+  console.log(equation);
+});
+
+let equalsButton = document.querySelector("#btn-equals"); 
+equalsButton.addEventListener("click", () => {
+  equation.push(calculatorValue);
+  for (let i = 0; i < equation.length-1; i = i + 2) { 
+    equation[i + 2] = operate(equation[i+1], equation[i], equation[i+2]);
+    console.log(equation);
+  }
+  display.textContent = equation[equation.length - 1];
 });
